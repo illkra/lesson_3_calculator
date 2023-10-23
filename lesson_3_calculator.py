515b847cd7ebca291fd487361251c574cc546d88
illkra <baruiga101@gmail.com>
2023-10-23 15:20:11 +0300

    lesson 3

--- lesson_3_calculator.py
+++ lesson_3_calculator.py
@@ -1,27 +1,42 @@
 value_int_1 = 0
 value_int_2 = 0
 result = 0
+choose = True
 
-try:
-    value_int_1 = int(input("Choose first type of number: "))
-    value_int_2 = int(input("Choose second type of number: "))
-except ValueError:
+while choose:
+ try:
+  value_int_1 = int(input("Choose first type of number: "))
+  value_int_2 = int(input("Choose second type of number: "))
+ except ValueError:
     print("Wrong type of number")
 
-value_operator = input("Choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/'\n : ")
+ value_operator = input("Choose an operator: \n 1 '+'\n 2 '-'\n 3 '*'\n 4 '/'\n : ")
 
-if value_operator == "1":
+ if value_operator == "1":
     result = value_int_1 + value_int_2
-elif value_operator == "2":
+ elif value_operator == "2":
     result = value_int_1 - value_int_2
-elif value_operator == "3":
+ elif value_operator == "3":
     result = value_int_1 * value_int_2
-elif value_operator == "4":
-    try:
-        result = value_int_1 / value_int_2
-    except ZeroDivisionError:
+ elif value_operator == "4":
+  try:
+    result = value_int_1 / value_int_2
+  except ZeroDivisionError:
         print("You cant divide on zero")
-else:
+  else:
     print("Incorrect operator")
 
-print(result)
+ print(result)
+
+ print("Do you want to use program again?")
+
+ choose = input("Answer 'Yes' or 'No'")
+
+ if choose == "No":
+  print("Thanks for using our product, have a nice day!")
+  choose = False
+  break
+ elif choose == "Yes":
+  continue
+ else:
+  print("Incorrect answer")
---
+++ lesson_4.py
@@ -0,0 +1,31 @@
+#value = 20
+#new_value = value / 2 if value < 100 else -value
+#print(new_value)
+
+####################### 1 #####################
+
+#value = 150
+#new_value = 1 if value < 100 else 0
+#print(new_value)
+
+###################### 2 ######################
+
+#value = 90
+#new_value = True if value < 100 else False
+#print(new_value)
+
+###################### 3 ######################
+
+#my_str = "qwe"
+#if len(my_str) < 5:
+#    my_str += my_str
+#print(my_str)
+
+##################### 4 #######################
+
+#my_str = "qwe"
+#if len(my_str) < 5:
+#   my_str += my_str[::-1]
+#print(my_str)
+
+#################### 5 ########################
