16bb591375ee8d7abf59ebff1862eb6ac7824bd6
illkra <baruiga101@gmail.com>
2023-11-05 12:57:56 +0200

    lesson 3

---
+++ hw_5.py
@@ -0,0 +1,30 @@
+#my_list = [85, 120, 90, 150, 60, 110, 105]
+
+#for number in my_list:
+   # if number > 100:
+       # print(number)
+
+########################### 1 ##########################
+
+#my_list = [85, 120, 90, 150, 60, 110, 105]
+#my_results = []
+
+#for number in my_list:
+    #if number > 100:
+        #my_results.append(number)
+
+#print(my_results)
+
+########################## 2 ##########################
+
+#my_list = [85, 120, 90]
+
+#if len(my_list) < 2:
+   # my_list.append(0)
+#else:
+    #my_list.append(my_list[-1] + my_list[-2])
+
+#print(my_list)
+
+########################## 3 #############################
+
